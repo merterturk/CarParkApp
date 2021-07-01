fa6e133b32b50e760c6a1354c0ea35fa75871bbb
Mert <meerterturk@gmail.com>
2021-07-02 01:48:20 +0300

    Added UserUI

---
+++ CarPark.User/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using CarPark.User.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CarPark.User.Controllers
+{
+   
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IStringLocalizer<HomeController> _localizer;
+
+        public HomeController(ILogger<HomeController> logger, IStringLocalizer<HomeController> localizer)
+        {
+            _logger = logger;
+            _localizer = localizer;
+        }
+
+        public IActionResult Index()
+        {
+            //var cultureInfo = CultureInfo.GetCultureInfo("en-US");
+            //Thread.CurrentThread.CurrentCulture = cultureInfo;
+            ////Thread.CurrentThread.CurrentUICulture = cultureInfo;
+
+            //var say_Hello_value = _localizer["Say_Hello"];
+            
+            // mongodb+srv://merterturk:Kuram123@carparkcluster.luztz.mongodb.net/CarParkDb?retryWrites=true&w=majority
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
